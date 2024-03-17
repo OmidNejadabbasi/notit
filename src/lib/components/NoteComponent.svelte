c4c44207403e91d87d4fdf8df033fbbb2197dbc8
Omid Nejadabbasi <omidne@systemgroup.net>
2024-03-17 23:19:43 +0330

    use dialog component

--- src/lib/components/NoteComponent.svelte
+++ src/lib/components/NoteComponent.svelte
@@ -5,7 +5,7 @@
     faSpinner,
   } from "@fortawesome/free-solid-svg-icons";
   import { debounceTime, Observable, Subject } from "rxjs";
-  import { onMount, untrack } from "svelte";
+  import { createEventDispatcher, onMount, untrack } from "svelte";
   import Fa from "svelte-fa";
   import { Note, NoteType } from "../data/Note";
   import { sl } from "../di";
@@ -22,12 +22,20 @@
   import { cssVariables } from "../utils/svelte-utils";
   import Flip from "gsap/dist/Flip";
   import { fade } from "svelte/transition";
+  import { createEditor, type EditorStore } from "../utils/editorjs.svelte";
+  import Dialog from "./shared/Dialog.svelte";
+
+  const { editor, isReady, data } = createEditor({ placeholder: "Empty Note" });
+  const dispatcher = createEventDispatcher();
 
   let formState: FormState = $state(new FormNewMode());
 
   let noteService: NoteService = sl.resolve(tNoteService);
   let { note = Note.newNote("", "") } = $props<{ note: Note }>();
-  let content = $state(note.content);
+  let content = $state(JSON.parse(note.content || "[]"));
+  isReady.subscribe((is) => {
+    if (is) data.set(content);
+  });
   let title = $state(note.title);
   let hasTitle = $derived(title?.length);
   let isFocused = $state(false);
@@ -41,7 +49,7 @@
   onMount(() => {
     if (note.id) formState = new FormSubmissionSucceed();
     contentChanges.subscribe(async ({ content, title }) => {
-      note.content = content;
+      note.content = JSON.stringify(content);
       note.title = title;
       try {
         if (!note.id) {
@@ -59,6 +67,24 @@
     });
     height = `${element.clientHeight}px`;
   });
+
+  function focusNote() {
+    isFocused = true;
+  }
+  function unfocusNote() {
+    isFocused = false;
+  }
+
+  let _e: EditorStore;
+  editor.subscribe((e) => {
+    _e = e;
+  });
+  function updateData() {
+    _e.save && _e.save();
+  }
+  data.subscribe((d) => {
+    content = d.blocks;
+  });
   let isModal = $state(false);
   $effect(() => {
     const prev = Flip.getState(element, {});
@@ -70,7 +96,6 @@
       Flip.from(prev, { duration: 0.4, ease: "power1.inOut" });
     });
   });
-
   $effect(() => {
     contentSubject.next({ content, title });
     formState = new FormDirty();
@@ -78,30 +103,28 @@
   });
 </script>
 
-<div
-  bind:this={fakeElement}
-  style="display: {isModal
-    ? 'block'
-    : 'none'}; min-height:{height}; width:{width}"
-  class="float-left"
-></div>
-{#if isFocused}
-  <div
-    class="overlay"
-    on:click={() => (isFocused = false)}
-    transition:fade
-  ></div>
-{:else}{/if}
-<!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
-  on:click={() => (isFocused = true)}
-  class="{isModal ? 'modal' : ''} card"
+  on:click={focusNote}
+  on:focusin={focusNote}
+  class="{isModal ? 'opacity-0' : ''} card transition-all duration-200"
   on:keydown={() => {}}
   bind:this={element}
   id="elem"
   use:cssVariables={{ height, width }}
 >
+  {#if hasTitle}
+    <!-- content here -->
+    <h3 class="input">{title}</h3>
+  {/if}
+  <div class="">
+    {title?.substring(0, 300) + (title?.length || 0 >= 300 ? "..." : "")}
+  </div>
+  {#if title?.length === 0}
+    <h3 class="text-gray-400">Empty Note</h3>
+  {/if}
+</div>
+<Dialog bind:isShowing={isFocused}>
   {#if hasTitle || isFocused}
     <!-- content here -->
     <input
@@ -109,11 +132,18 @@
       bind:value={title}
       class="input focus:border-none"
       placeholder="Title"
-      in:scaleHeight={{ duration: 300 }}
-      out:scaleHeight={{ duration: 300 }}
     />
   {/if}
-  <div bind:innerHTML={content} contenteditable="true" class="content" />
+  <div
+  class:hidden={!isFocused}
+    use:editor={{
+      onChange: (ev) => {
+        updateData();
+      },
+    }}
+    class="content"
+    dir="ltr"
+  ></div>
   <div class="absolute bottom-2 right-2">
     {#if formState instanceof FormSubmissionSucceed}
       <Fa icon={faCircleCheck} color="limegreen" />
@@ -130,17 +160,20 @@
       <p>Fuck js</p>
     {/if}
   </div>
-</div>
+</Dialog>
 
 <style lang="scss">
   .input {
     @apply outline-none font-semibold text-lg flex-none;
   }
   .content {
-    @apply border-none outline-none min-h-[46px] w-full text-gray-900 flex-1;
+    @apply border-none outline-none min-h-[46px] text-gray-900;
     text-overflow: inherit;
     overflow: inherit;
     white-space: inherit;
+    scrollbar-width: thin;
+    min-width: 720px;
+    max-width: 90vw;
   }
   .card {
     @apply rounded-md shadow-md w-full border-[1px] outline-none p-2 max-w-xl hover:border-2 hover:border-gray-400 focus-within:border-2
@@ -153,7 +186,7 @@
   .modal {
     position: fixed;
     max-height: 80% !important;
-    width: 750px !important;
+    min-width: 750px !important;
     background-color: white;
     // left: calc(50vw - var(--width) / 2);
     left: 50px;
@@ -162,16 +195,5 @@
     text-overflow: unset !important;
     overflow-y: auto !important;
     white-space: unset !important;
-  }
-
-  .overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background-color: #202124;
-    opacity: 0.42;
-    z-index: 10;
   }
 </style>
--- src/lib/components/shared/Dialog.svelte
+++ src/lib/components/shared/Dialog.svelte
@@ -1,14 +1,53 @@
 <script lang="ts">
+  import { cssVariables } from "../../utils/svelte-utils";
+  import { fade } from "svelte/transition";
+
   export let isShowing: boolean;
 
-  let left;
+  let left, width;
   let top;
+  $: widthVar=`${width}px`
 </script>
 
 <div
-  class="absolute w-40 h-min top-0 left-0 bg-white {isShowing
-    ? 'hidden'
-    : 'flex'}"
+  class="wrapper"
+  class:showing={isShowing}
+  bind:clientWidth={width}
+  use:cssVariables={{ widthVar }}
 >
   <slot />
 </div>
+
+{#if isShowing}
+  <div
+    class="overlay"
+    on:click={() => (isShowing = false)}
+    transition:fade
+  ></div>
+{:else}{/if}
+
+<style>
+  .showing {
+    display: block !important;
+  }
+
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: #202124;
+    opacity: 0.42;
+    z-index: 10;
+  }
+  .wrapper{
+    @apply rounded-md p-3 bg-white;
+    position: absolute;
+    left: calc(50vw - var(--widthVar) / 2);
+    top: 100px;
+    max-height: 80%;
+    z-index: 20;
+    display: none;
+  }
+</style>
